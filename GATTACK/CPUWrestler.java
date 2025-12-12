06976cc60f21a60c7d9878c4230e5d23e9a5ffec
SoniaN12 <168844622+SoniaN12@users.noreply.github.com>
2025-12-12 12:21:19 +0100

    Add files via upload

---
+++ GATTACK/CPUWrestler.java
@@ -0,0 +1,57 @@
+import java.util.Random;
+class CPUWrestler extends Wrestler {
+
+    public CPUWrestler() {
+        super("CPU", 100, 200); // Start with 200 stamina , same as player
+    }
+
+    @Override
+    public void takeTurn(Wrestler opponent) {
+        // CPU randomly selects a move it has enough stamina for
+        Wrestler.Move[] moves = Wrestler.Move.values();
+        Random random = new Random();
+        Wrestler.Move move;
+
+        // Keep selecting until a move CPU can perform is found
+        do {
+            move = moves[random.nextInt(moves.length)];
+        } while (!canPerform(move));
+
+        switch (move) {
+            case LIGHT_ATTACK -> lightAttack(opponent);
+            case HEAVY_ATTACK -> heavyAttack(opponent);
+            case BLOCK -> block();
+            case RECOVER -> recover();
+        }
+    }
+
+    private boolean canPerform(Wrestler.Move move) {
+        return switch (move) {
+            case LIGHT_ATTACK -> stamina >= 10;
+            case HEAVY_ATTACK -> stamina >= 25;
+            default -> true; // block and recover always possible
+        };
+    }
+
+    public void lightAttack(Wrestler opponent) {
+        int damage = random.nextInt(8) + 5;
+        opponent.takeDamage(damage);
+    }
+
+    public void heavyAttack(Wrestler opponent) {
+        int damage = random.nextInt(13) + 10;
+        opponent.takeDamage(damage);
+    }
+
+    public void block() {
+        blocking = true;
+        stamina += 5;
+        if (stamina > 50) stamina = 50; // cap at max stamina
+    }
+
+    public void recover() {
+        int recovery = random.nextInt(10) + 5;
+        stamina += recovery;
+        if (stamina > 50) stamina = 50;
+    }
+}
---
+++ GATTACK/PlayerWrestler.java
@@ -0,0 +1,33 @@
+class PlayerWrestler extends Wrestler {
+
+        public PlayerWrestler() {
+                super("Player", 200, 200); // Start at 200, consistent with MoveHandler
+        }
+
+        @Override
+        public void takeTurn(Wrestler opponent) {
+                // GUI triggers moves via buttons
+        }
+
+        public void lightAttack(Wrestler opponent) {
+                int damage = random.nextInt(8) + 5;
+                opponent.takeDamage(damage);
+        }
+
+        public void heavyAttack(Wrestler opponent) {
+                int damage = random.nextInt(13) + 10;
+                opponent.takeDamage(damage);
+        }
+
+        public void block() {
+                blocking = true;
+                stamina += 5;
+                if (stamina > 50) stamina = 50; // Cap stamina
+        }
+
+        public void recover() {
+                int recovery = random.nextInt(10) + 5;
+                stamina += recovery;
+                if (stamina > 50) stamina = 50; // Cap stamina
+        }
+}
